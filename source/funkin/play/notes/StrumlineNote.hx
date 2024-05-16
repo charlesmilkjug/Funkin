0dbaded9beded0be0bd3f28e285fd856270481d1
CharlesCatYT <sptubea@gmail.com>
2024-05-16 19:00:37 -0400

    make strum confirm anim. lengths more consistent

--- source/funkin/play/notes/StrumlineNote.hx
+++ source/funkin/play/notes/StrumlineNote.hx
@@ -16,7 +16,7 @@ class StrumlineNote extends FlxSprite
 
   var confirmHoldTimer:Float = -1;
 
-  static final CONFIRM_HOLD_TIME:Float = 0.1;
+  static final CONFIRM_HOLD_TIME:Float = 0.15;
 
   function set_direction(value:NoteDirection):NoteDirection
   {
@@ -48,10 +48,12 @@ class StrumlineNote extends FlxSprite
     // Run a timer before we stop playing the confirm animation.
     // On opponent, this prevent issues with hold notes.
     // On player, this allows holding the confirm key to fall back to press.
-    if (name == 'confirm')
-    {
-      confirmHoldTimer = 0;
-    }
+    /*
+      if (name == 'confirm')
+        {
+          confirmHoldTimer = 0;
+      }
+     */
   }
 
   override function update(elapsed:Float)
@@ -109,6 +111,11 @@ class StrumlineNote extends FlxSprite
   {
     this.active = true;
     this.playAnimation('confirm', true);
+
+    // Run a timer before when we play the confirm animation.
+    // On opponent, this prevent issues with hold notes.
+    // On player, this allows holding the confirm key to fall back to press.
+    confirmHoldTimer = 0;
   }
 
   public function isConfirm():Bool
